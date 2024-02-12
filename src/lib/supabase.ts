d3b0be5a0bd66072e25af39bb409143a15909832
Bilal-Ghiffari <bilalalghiffari53@gmail.com>
2024-02-12 16:14:54 +0700

    integration preparation prisma and databases

---
+++ src/lib/supabase.ts
@@ -0,0 +1,29 @@
+import { createClient } from "@supabase/supabase-js";
+import { randomId } from "./utils";
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!!,
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLIC_KEY!!
+);
+
+export const supabasePublicUrl = async (filename: string, bucket: string) => {
+  const {
+    data: { publicUrl },
+  } = await supabase.storage.from(bucket).getPublicUrl(`public/${filename}`);
+  return publicUrl;
+};
+
+export const supabaseUploadFile = async (
+  file: File | string,
+  bucket: string
+) => {
+  const filename = `resume-${randomId(5)}.pdf`;
+  const { data, error } = await supabase.storage
+    .from(bucket)
+    .update(`public/${filename}`, file, {
+      cacheControl: "3600",
+      upsert: false,
+    });
+
+  return { data, filename, error };
+};
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,35 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import bcrypt from "bcryptjs";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const randomId = (length: number) => {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUFWXYZabcdefghijklmnopqrstufwxyz12345678910";
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+};
+
+export const hashPassword = async (password: string) => {
+  if (password) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    return hashedPassword;
+  }
+};
+
+export const comparePassword = async (
+  password: string,
+  hashedPassword: string
+) => {
+  const isMatch = await bcrypt.compare(password, hashedPassword);
+  return isMatch;
+};
